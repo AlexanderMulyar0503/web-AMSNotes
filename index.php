4592be977e8d1b0c81a36c3b3df6b4b8d93dddbb
Alexander Mulyar <mulyar.alexander0503@yandex.ru>
2024-05-23 19:57:11 +0500

    Adding a header and footer. Adding notesBox.

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
     include "conf.php";
+
+    $notesArray = scandir($CONF["pathNotes"]);
 ?>
 
 <!DOCTYPE html>
@@ -8,15 +10,38 @@
         <title> <?php print($CONF["company"]); ?> </title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="img/favicon.png">
+        <link rel="stylesheet" href="css/main.css">
     </head>
 
     <body>
         <header>
-            <?php print($CONF["company"]); ?>
+            <div class="header_logo">
+                <a href="./"> <img src="img/favicon.png" width="75px" height="75px"> </a>
+            </div>
+
+            <div class="header_link">
+                <a href="./"> <b> <i> <?php print($CONF["company"]); ?> </i> </b> </a>
+            </div>
         </header>
 
+        <div class="notesBox">
+            <?php
+                for ($i = 2; $i < count($notesArray); $i++)
+                {
+                    print("<p>---------</p>");
+                    print("<p><b>" . basename($notesArray[$i], ".txt") . "</b></p>");
+                    $readFile = file_get_contents($CONF["pathNotes"] . "/" . $notesArray[$i]);
+                    print("<p>" . $readFile . "</p>");
+                    print("<p>---------</p>");
+                }
+            ?>
+        </div>
+
         <footer>
-            <?php print($CONF["copyright"]); ?>            
+            <p>
+                <?php print($CONF["copyright"]); ?>
+            </p>
         </footer>
     </body>
 </html>
